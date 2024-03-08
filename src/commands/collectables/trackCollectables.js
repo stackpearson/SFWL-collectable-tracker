56db8e88fd1eb976e9d70ab61b9e8d58e9d7e290
Sawyer <sawyer.pearson92@gmail.com>
2024-03-08 14:52:15 -0700

    adding role tracking

--- src/commands/collectables/trackCollectables.js
+++ src/commands/collectables/trackCollectables.js
@@ -2,6 +2,15 @@ const { SlashCommandBuilder } = require("discord.js");
 const writeToSheet = require("../../functions/postToGsheet");
 require('dotenv').config();
 
+const collectableRoles = {
+    'pinup': '1180641401831166014',
+    'scifi': '1180641401831166014',
+    'western': '1180641401831166014',
+    'lewd-scifi': '1204969563528241212',
+    'lewd-zero': '1182158742159114250',
+    'pablito-mat': '1182158216621199432'
+}
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('collectables')
@@ -27,10 +36,17 @@ module.exports = {
 
             const collectableSheetName = await interaction.options.getString("collectable");
             const user = await interaction.options.getUser('user');
+            const roleId = collectableRoles[collectableSheetName];
+            const member = await interaction.guild.members.fetch(user.id);
+
+            if (!member.roles.cache.has(roleId)) {
+                await member.roles.add(roleId)
+            } console.log('user role already applied')
+
             const tryWrite = await writeToSheet(user.id, collectableSheetName);
 
             if (tryWrite) {
-                await interaction.reply({ content: `${collectableSheetName} successfully tracked for ${user.username}! https://tenor.com/bbiKC.gif`, ephemeral: false });
+                await interaction.reply({ content: `${collectableSheetName} successfully tracked & role applied (if applicable) for ${user.username}! https://tenor.com/bbiKC.gif`, ephemeral: false });
             } else {
                 await interaction.reply({ content: `${user.username} has already met the cap for ${collectableSheetName}s https://tenor.com/bczMQ.gif`, ephemeral: false });
             }
