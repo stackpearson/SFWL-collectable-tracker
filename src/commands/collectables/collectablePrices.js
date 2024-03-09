cb0e556d7bb48d3b93e3da3447f3b6341c0d9425
Sawyer Pearson <sawyer.pearson208@gmail.com>
2024-03-09 13:57:08 -0800

    typo

--- src/commands/collectables/collectablePrices.js
+++ src/commands/collectables/collectablePrices.js
@@ -7,7 +7,7 @@ module.exports = {
         .setDescription('See breakdown of collectable prices'),
     async execute(interaction) {
         try {
-            await interaction.reply({ content: '__**Scifi Mats: 18 count w/ shelf**__\nLimits: 2x per season\nPrize: $80M\n\n__**Fallout Lunchboxes: 100 cards each**__\n*Scifi | Western | Pinup*\nLimits: 2x per season\nPrize: $60M\n\n__**Scifi Lewds: 100 cards**__\nLimits: 2x per season\nPrize: $60M\n\n__**Zeros Waifus: 200 cards**__\nLimits: 2x per season\nPrize: $120M\n\n', ephemeral: false });
+            await interaction.reply({ content: '__**Scifi Mats: 18 count w/ shelf**__\nLimits: 2x per season\nPrize: $80M\n\n__**Fallout Lunchboxes: 100 cards each**__\n*Scifi | Western | Pinup*\nLimits: 2x per season\nPrize: $60M\n\n__**Scifi Lewds: 200 cards**__\nLimits: 2x per season\nPrize: $60M\n\n__**Zeros Waifus: 200 cards**__\nLimits: 2x per season\nPrize: $120M\n\n', ephemeral: false });
         } catch (error) {
             console.error('Error in collectable price check:', error.message);
             await interaction.reply({ content: error.message, ephemeral: true });
